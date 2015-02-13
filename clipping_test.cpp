c5c8b51bf169622249a6b595d94d12ee00a91717
artemp <artem@mapnik.org>
2015-02-13 18:25:59 +0100

    update clipping test to read wkt from a file
    ./bin/clang-darwin-4.2.1/release/clipping-test ../mapnik_deps/boost_1_57_0/libs/geometry/example/data/world.wkt "BOX(-10 -10,10 40)" 100

--- clipping_test.cpp
+++ clipping_test.cpp
@@ -21,6 +21,7 @@
  *****************************************************************************/
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <cassert>
 
@@ -31,71 +32,165 @@
 #include "geometry_impl.hpp"
 #include "geometry_adapters.hpp"
 
+
+namespace boost { namespace geometry {
+
+template <>
+struct point_type<mapnik::util::variant<mapnik::new_geometry::polygon3, mapnik::new_geometry::multi_polygon> >
+{
+    using type = mapnik::new_geometry::point;
+};
+
+
+} // namespace geometry
+} // namespace boost
+
+template <typename Geometry, typename Box>
+inline void read_wkt(std::string const& filename, std::vector<Geometry>& geometries, Box& box)
+{
+    std::ifstream file(filename.c_str());
+    if (file.is_open())
+    {
+        while (! file.eof() )
+        {
+            std::string line;
+            std::getline(file, line);
+            if (! line.empty())
+            {
+                mapnik::new_geometry::polygon3 poly;
+                try
+                {
+                    boost::geometry::read_wkt(line, poly);
+                    geometries.push_back(poly);
+                    boost::geometry::expand(box, boost::geometry::return_envelope<Box>(poly));
+                }
+                catch (boost::geometry::exception const& ex)
+                {
+                    mapnik::new_geometry::multi_polygon multi_poly;
+                    try
+                    {
+                        boost::geometry::read_wkt(line, multi_poly);
+                        geometries.push_back(multi_poly);
+                        boost::geometry::expand(box, boost::geometry::return_envelope<Box>(multi_poly));
+                    }
+                    catch (boost::geometry::exception const& ex)
+                    {
+                        std::cerr << ex.what() << std::endl;
+                    }
+                }
+            }
+        }
+    }
+}
+
+template <typename Box, typename PolygonList>
+struct intersection
+{
+    intersection (Box const& clip_box, PolygonList & clipped_polygons)
+        : clip_box_(clip_box), clipped_polygons_(clipped_polygons) {}
+
+    template <typename Geometry>
+    void apply(Geometry const& geom)
+    {
+        mapnik::util::apply_visitor(*this, geom);
+    }
+
+    void operator() (mapnik::new_geometry::polygon3 const& poly)
+    {
+        boost::geometry::intersection(clip_box_, poly, clipped_polygons_);
+    }
+
+    void operator() (mapnik::new_geometry::multi_polygon const& multi_poly)
+    {
+        for (auto const& poly : multi_poly)
+        {
+            //std::cerr << boost::geometry::wkt(poly) << std::endl;
+            boost::geometry::intersection(clip_box_, poly, clipped_polygons_);
+        }
+    }
+
+    template <typename T>
+    void operator() (T const& g)
+    {
+        std::cerr << typeid(g).name() << std::endl;
+    }
+    Box const& clip_box_;
+    PolygonList & clipped_polygons_;
+};
+
 int main(int argc, char ** argv)
 {
     using polygon_list = std::vector<mapnik::new_geometry::polygon3>;
+    using geometry = mapnik::util::variant<mapnik::new_geometry::point,
+                                           mapnik::new_geometry::line_string,
+                                           mapnik::new_geometry::polygon3,
+                                           mapnik::new_geometry::multi_polygon>;
 
     std::cerr << "Clipping test" << std::endl;
     std::cerr << "Boost.geometry" << std::endl;
 
     if (argc != 4)
     {
-        std::cerr << "Usage:" << argv[0] << "<polygon-wkt> <bbox-wkt> <num-iterations>" << std::endl;
+        std::cerr << "Usage:" << argv[0] << " <wkt-filename> <bbox-wkt> <num-iterations>" << std::endl;
         return EXIT_FAILURE;
     }
-    std::string poly_wkt(argv[1]);
+
+    std::string wkt_filename(argv[1]);
     std::string bbox_wkt(argv[2]);
     std::size_t num_iterations = std::stol(argv[3]);
     std::cerr << "NUM_ITERATIONS=" << num_iterations << std::endl;
     mapnik::new_geometry::bounding_box clip_box;
-    mapnik::new_geometry::polygon3 input_poly;
+    boost::geometry::read_wkt(bbox_wkt, clip_box);
+    std::vector<geometry> geometries;
+    mapnik::new_geometry::bounding_box bbox;
+    read_wkt(wkt_filename, geometries , bbox);
+    boost::timer::auto_cpu_timer t;
 
-    try
+    for (std::size_t i = 0; i < num_iterations ; ++i)
     {
-        boost::geometry::read_wkt(poly_wkt, input_poly);
-        boost::geometry::read_wkt(bbox_wkt, clip_box);
-    }
-    catch (boost::geometry::exception const& ex)
-    {
-        std::cerr << ex.what() << std::endl;
-    }
-
-    std::cerr << "Area: " << boost::geometry::area(input_poly) << std::endl;
-    std::cerr << "WKT A: " << boost::geometry::wkt(input_poly) << std::endl;
-    std::cerr << "WKT B: " << boost::geometry::wkt(clip_box) << std::endl;
-    std::cerr << "Is valid? :" << std::boolalpha << boost::geometry::is_valid(input_poly) << std::endl;
-    std::cerr << "Is simple? :" << std::boolalpha << boost::geometry::is_simple(input_poly) << std::endl;
-
-    {
-        boost::timer::auto_cpu_timer t;
-        std::size_t count = 0;
-        for (std::size_t i = 0; i < num_iterations ; ++i)
+        if (i == 0)
         {
-            polygon_list clipped_polygons;
-            try
-            {
-                boost::geometry::intersection(input_poly, clip_box, clipped_polygons);
-                for (auto const& p : clipped_polygons)
-                {
+            std::cerr << "NUM GEOMETRIES = " << geometries.size() << std::endl;
+        }
 
-                    if (i == 0)
+        for (auto geom : geometries)
+        {
+
+            //std::cerr << "Area: " << boost::geometry::area(geom) << std::endl;
+            //std::cerr << "WKT A: " << boost::geometry::wkt(geom) << std::endl;
+            //std::cerr << "WKT B: " << boost::geometry::wkt(clip_box) << std::endl;
+            //std::cerr << "Is valid? :" << std::boolalpha << boost::geometry::is_valid(geom) << std::endl;
+            //std::cerr << "Is simple? :" << std::boolalpha << boost::geometry::is_simple(geom) << std::endl;
+
+            {
+
+                std::size_t count = 0;
+                polygon_list clipped_polygons;
+                try
+                {
+                    intersection<mapnik::new_geometry::bounding_box, polygon_list> op(clip_box, clipped_polygons);
+                    op.apply(geom);
+                    for (auto const& p : clipped_polygons)
                     {
-                        //std::cerr << "Is valid? :" << std::boolalpha << boost::geometry::is_valid(p) << std::endl;
-                        std::cerr << boost::geometry::wkt(p) << std::endl;
-                    }
-                    count += p.exterior_ring.size();
-                    for (auto const& ring :  p.interior_rings)
-                    {
-                        count += ring.size();
+                        if (i == 0)
+                        {
+                            //std::cerr << "Is valid? :" << std::boolalpha << boost::geometry::is_valid(p) << std::endl;
+                            std::cout << boost::geometry::wkt(p) << std::endl;
+                        }
+                        //count += p.exterior_ring.size();
+                        //for (auto const& ring :  p.interior_rings)
+                        //{
+                        //    count += ring.size();
+                        //}
                     }
                 }
+                catch (boost::geometry::exception const& ex)
+                {
+                    std::cerr << ex.what() << std::endl;
+                }
             }
-            catch (boost::geometry::exception const& ex)
-            {
-                std::cerr << ex.what() << std::endl;
-            }
+            //std::cerr << "count=" << count << std::endl;
         }
-        std::cerr << "count=" << count << std::endl;
     }
     return EXIT_SUCCESS;
 }
